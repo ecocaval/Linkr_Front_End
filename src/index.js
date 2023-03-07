22a340894ebb574ad556f11382d1d6eb1d9808a5
erico <ecocaval@hotmail.com>
2023-03-07 14:49:59 -0300

    Feat: adding header and page background
    Also adding a folder for global css variables

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+import GlobalColors from './styles/GlobalColors';
 import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
+    <GlobalColors />
     <App />
   </React.StrictMode>
 );
