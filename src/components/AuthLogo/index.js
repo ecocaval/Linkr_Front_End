db11bd3f7a979e6c3ac9c5fd6b8fd3db29aa0ae6
FabioGarroteDev <fgarrote12@gmail.com>
2023-03-08 03:07:42 -0300

    Feat: adding login page with axios call

---
+++ src/components/AuthLogo/index.js
@@ -0,0 +1,11 @@
+import { AuthLogoContainer } from "./style.js";
+
+export default function AuthLogo() {
+  return(
+    <AuthLogoContainer>
+      <h1>linkr</h1>
+      <h2>save, share and discover</h2>
+      <h2>the best links on the web</h2>
+    </AuthLogoContainer>
+  )
+}
