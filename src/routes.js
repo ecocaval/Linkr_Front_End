603a2796e2669af0b0155a155cac05fd6fd63d9a
erico <ecocaval@hotmail.com>
2023-03-08 19:10:47 -0300

    Fix: header and browserRoutes
    Also commenting Home path, discomment when creating Home component

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import LoginPage from "./src/pages/LoginPage";
+import LoginPage from "./pages/LoginPage";
 
-export default () => {
+export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                {/* <Route path="/" element={<Home />} /> */}
                 <Route path="/signin" element={<LoginPage />}/>
                 <Route path="*" element={<NotFound/>} />
             </Routes>
