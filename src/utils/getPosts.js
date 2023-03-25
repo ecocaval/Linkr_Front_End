8b0d13babced4c15b1f66b93c6457fba0529ff20
erico <ecocaval@hotmail.com>
2023-03-25 17:26:35 -0300

    fix: checkForDuplicityInPosts in getPosts()

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -2,18 +2,38 @@ import axios from "axios";
 
 export default async function getPosts(setPosts, setSendPost) {
     const token = localStorage.getItem('token')
-    if(!token) return    
+    if (!token) return
     try {
         const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/posts`, {
             headers: {
                 authorization: `Bearer ${token}`
             }
         })
-        setPosts(data)
+        setPosts(checkForDuplicityInPosts(data))
         if (!!setSendPost) setSendPost(false) //* used in the loader button in the publish post modal
         return true
     } catch (error) {
         console.error(error)
     }
     return false
+}
+
+export function checkForDuplicityInPosts(posts) {
+    const postsHashTable = {}
+    let postsCopy = [...posts]
+    let indexesToRemove = []
+
+    for (let i = 0; i < posts.length; i++) {
+        if (postsHashTable[posts[i].postId]) {
+            indexesToRemove.push(i)
+        }
+        else postsHashTable[posts[i].postId] = true
+    }
+    if (indexesToRemove.length) {
+        indexesToRemove.forEach(index => {
+            postsCopy = postsCopy.splice(index, 1)
+        })
+        return postsCopy
+    }
+    return posts
 }
