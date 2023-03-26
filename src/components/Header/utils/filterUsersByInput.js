7f3ddf4e3c9035149b38c5e6b951fd5a0db664a5
erico <ecocaval@hotmail.com>
2023-03-25 23:57:35 -0300

    feature: adding following priority

--- src/components/Header/utils/filterUsersByInput.js
+++ src/components/Header/utils/filterUsersByInput.js
@@ -1,5 +1,10 @@
 export default function filterUsersByInput(usersInSearch, headerInputValue) {
     return usersInSearch.filter(user => (
         user.name.toLowerCase().includes(headerInputValue.toLowerCase()) && headerInputValue !== ""
-    ))
+    )).sort((a, b) => {
+        if (a.imFollowing && b.imFollowing) return 0
+        else if (a.imFollowing) return -1
+        else if (b.imFollowing) return 1
+        return 0
+    })
 }
